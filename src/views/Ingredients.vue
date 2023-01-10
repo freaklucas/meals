8a5e51d571d0a26db12266757a5c346e6ebfa955
freaklucas <lucas.oliveira.freak@gmail.com>
2023-01-10 14:42:23 -0300

    search ok

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-sans mb-4">Ingredients</h1>
+      <input
+        v-model="keyword"
+        class="rounded border-2 mb-3 
+          border-gray-200 w-full"
+        type="text"
+        placeholder="Search for ingredients" 
+      />
     <router-link
-      v-for="ingredient of ingredients"
       :to="{ 
         name: 'byIngredient', 
         params: {ingredient: ingredient.strIngredient},
       }"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.id"
       class="block bg-white roundend 
         p-3 mb-3 shadow hover:bg-purple-50
@@ -23,10 +30,17 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "@vue/runtime-core";
+import { computed, onMounted, ref } from "@vue/runtime-core";
 import axiosClient from "../axiosClient";
 
+const keyword = ref('');
 const ingredients = ref([]);
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) =>
+    i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
+  );
+});
 
 onMounted(() => {
   axiosClient.get('list.php?i=list')
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -14,19 +14,7 @@
       </router-link>
     </div>
   </div>
-  <div 
-    class="
-      grid grid-cols-1 
-      md:grid-cols-3 gap-5
-      p-8
-    "
-  >
-    <MealItem 
-      v-for="meal of meals" 
-      :key="meal.idMeal" 
-      :meal="meal"  
-    />
-  </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
@@ -35,7 +23,7 @@ import { onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import store from '../store';
 
-import MealItem from '../components/MealItem.vue';
+import Meals from '../components/Meals.vue';
 
 const route = useRoute();
 const letters = 'ABCDEFGHIJQKLMNOPQRSTUVWXYZ'
